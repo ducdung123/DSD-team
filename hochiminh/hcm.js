d0679da443b8e06c2167fe04fcd0590f18cbeac2
LAPTOP-N9O8BIEU\duc dung <escape>
2020-09-14 00:49:41 +0700

    no message

--- hochiminh/hcm.js
+++ hochiminh/hcm.js
@@ -5,6 +5,7 @@ function overview(){
     }
     document.getElementById("nav1").classList.remove("navremove")
     document.getElementById("nav1").classList.add("navadd")
+    document.getElementById("dropdownMenuButton").innerHTML="overview"
 }
 function gallery(){
     for(var i=1; i<=4; i++){
@@ -16,7 +17,7 @@ function gallery(){
     }
     document.getElementById("nav2").classList.remove("navremove")
     document.getElementById("nav2").classList.add("navadd")
-
+    document.getElementById("dropdownMenuButton").innerHTML="gallery"
 }
 function alsolike(){
     for(var i=1; i<=4; i++){
@@ -29,7 +30,7 @@ function alsolike(){
     }
     document.getElementById("nav3").classList.remove("navremove")
     document.getElementById("nav3").classList.add("navadd")
-
+    document.getElementById("dropdownMenuButton").innerHTML="You may also like"
 }
 function nearplace(){
     for(var i=1; i<=4; i++){
@@ -42,5 +43,5 @@ function nearplace(){
     }
     document.getElementById("nav4").classList.remove("navremove")
     document.getElementById("nav4").classList.add("navadd")
-
+    document.getElementById("dropdownMenuButton").innerHTML="Nearby"
 }
